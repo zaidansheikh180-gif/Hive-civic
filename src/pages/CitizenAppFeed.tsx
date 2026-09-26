985b3ff3a9207566fa8736d0d4450897bca44c31
fateh <232430729+zaidansheikh180-gif@users.noreply.github.com>
2026-09-26 19:07:14 +0530

    Increase OrbNoise size in loading state

--- src/pages/CitizenAppFeed.tsx
+++ src/pages/CitizenAppFeed.tsx
@@ -291,7 +291,7 @@ export const CitizenAppFeed: React.FC = () => {
       {loading ? (
         <div className="min-h-[40vh] flex items-center justify-center">
           <div className="flex flex-col items-center gap-3">
-            <OrbNoise width={48} height={48} density={90} speed={28} pointer={{ drag: 0 }} />
+            <OrbNoise width={72} height={72} density={90} speed={28} pointer={{ drag: 0 }} />
             <span className="text-xs font-mono text-[#CC9E33]">Loading proposals...</span>
           </div>
         </div>
